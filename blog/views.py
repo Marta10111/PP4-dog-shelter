67c1bf001c60ad3df318fb11e55c3e5eac6fd625
Marta <manta.101@hotmail.com>
2023-03-17 16:13:19 +0000

    Adding content to blog as admin

--- blog/views.py
+++ blog/views.py
@@ -68,11 +68,12 @@ class PostDetail(View):
 
 class PostLike(View):
     
-    def post(self, request, slug):
+    def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
-
+        
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
         else:
             post.likes.add(request.user)
-        return HttpResponseRedirect(reverse('post_detail', args=[slug]))   
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
